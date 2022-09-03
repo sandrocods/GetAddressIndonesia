3633f249fe95e6f573072f3e023870c18ccdbeba
sandrocods <59155826+sandrocods@users.noreply.github.com>
2022-09-04 00:20:15 +0700

    add test

--- main.py
+++ main.py
@@ -2,7 +2,34 @@ from database import Database
 
 database = Database('data.db')
 
+print("==" * 20 + " Search by kode pos " + "==" * 20)
+# Search by kode pos
 print(
     database.search_multi('57211')
 
+)
+
+print("==" * 20 + " Search by kelurahan " + "==" * 20)
+# search by kelurahan
+print(
+    database.search_multi('Sragen Tengah')
+
+)
+
+print("==" * 20 + " Search by kecamatan " + "==" * 20)
+# search by kecamatan
+print(
+    database.search_multi('Sragen')
+)
+
+print("==" * 20 + " Search by kabupaten " + "==" * 20)
+# search by kabupaten
+print(
+    database.search_multi('Sragen')
+)
+
+print("==" * 20 + " Search by desa " + "==" * 20)
+# search by desa
+print(
+    database.search_multi('Pilangsari')
 )
